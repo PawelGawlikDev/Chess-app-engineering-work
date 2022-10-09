c0e47f8b010240b70aabd74a8d97c4667bc7af04
Paweł Gawlik (253408) <253408@student.pwr.edu.pl>
2022-10-09 22:39:57 +0200

    New version

---
+++ chessapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Partie
+
+# Register your models here.
+
+admin.site.register(Partie)
+
---
+++ chessapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Partie
+
+def index(request):
+    curent_user = request.user
+    user = Partie.objects.filter(user=curent_user.id)
+    dane = {'user': user}
+    return render(request, "games.html", dane)
+
+
+def partie(request, id):
+    partie_user = Partie.objects.get(pk=id)
+    dane = {'partie_user': partie_user}
+    return render(request, "board.html", dane)
